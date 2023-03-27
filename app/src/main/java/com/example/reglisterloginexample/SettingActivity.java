3f613e96c6c55be55a62a1c64c1ff42264fac88c
school <kyun212k@gmail.com>
2023-03-27 20:49:45 +0900

    2일차 로그아웃 구현 및 회원 정보 수정 50% 완성, 회원 탈퇴 구성 중

--- app/src/main/java/com/example/reglisterloginexample/SettingActivity.java
+++ app/src/main/java/com/example/reglisterloginexample/SettingActivity.java
@@ -4,26 +4,28 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SettingActivity extends Activity {
+public class SettingActivity extends UpdateActivity {
     private TextView tv_id, tv_pass;
+    private Button btn_update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        tv_id = findViewById(R.id.tv_id);
-        tv_pass = findViewById(R.id.tv_pass);
-
-        Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-        String userPass = intent.getStringExtra("userPass");
-
-        tv_id.setText(userID);
-        tv_pass.setText(userPass);
-
+        btn_update = (Button) findViewById(R.id.btn_update);
+        btn_update.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, UpdateActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/reglisterloginexample/UpdateActivity.java
+++ app/src/main/java/com/example/reglisterloginexample/UpdateActivity.java
@@ -1,4 +1,63 @@
 package com.example.reglisterloginexample;
 
-public class UpdateActivity extends {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+
+public class UpdateActivity extends Activity {
+    private static final int CALL_SUBACTIVITY =0;
+    EditText ex_id, ex_pass, ex_name, ex_age;
+
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+
+    public void onButtonClick(View view) {
+        ex_id = findViewById(R.id.ex_id);
+        String ID = ex_id.getText().toString();
+
+        ex_pass = findViewById(R.id.ex_pass);
+        String Pass = ex_pass.getText().toString();
+
+        ex_name = findViewById(R.id.ex_name);
+        String Name = ex_name.getText().toString();
+
+        ex_age = findViewById(R.id.ex_age);
+        String Age = ex_age.getText().toString();
+
+    Intent intent = new Intent(this.getApplicationContext(), UpdateSubActivity.class);
+        intent.putExtra("ID", ID);
+        intent.putExtra("Pass", Pass);
+        intent.putExtra("Name", Name);
+        intent.putExtra("Age", Age);
+
+        startActivityForResult(intent, CALL_SUBACTIVITY);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case CALL_SUBACTIVITY:
+                if(requestCode == RESULT_OK) {
+                    String ID = data.getStringExtra("ID");
+                    String Pass = data.getStringExtra("Pass");
+                    String Name = data.getStringExtra("Name");
+                    String Age = data.getStringExtra("Age");
+
+                    ex_id.setText(ID);
+                    ex_pass.setText(Pass);
+                    ex_name.setText(Name);
+                    ex_age.setText(Age);
+                }
+                break;
+        }
+
+
+    }
 }
